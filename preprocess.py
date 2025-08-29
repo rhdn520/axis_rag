851166e0aac29ca3f0821898908ebe00a5ebd874
rhdn520 <520gow@gmail.com>
2025-08-29 09:32:52 +0900

    added synonym list

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,10 @@
 import os
-os.environ["HF_HOME"] = "/home/seungwoochoi/data/huggingface/cache"
+from dotenv import load_dotenv
+load_dotenv()
+hf_home = os.getenv("HF_HOME")
+if hf_home:
+    os.environ["HF_HOME"] = hf_home
+
 import numpy as np
 from datasets import load_dataset
 from FlagEmbedding import BGEM3FlagModel
@@ -12,6 +17,6 @@ for data in marco_data:
 
 model = BGEM3FlagModel('BAAI/bge-m3', use_fp16=True) 
 
-passage_embeddings = model.encode(passages, batch_size=32, max_length=8192, return_dense=True)
+passage_embeddings = model.encode(passages, batch_size=2, max_length=8192, return_dense=True)
 
 np.save("/home/seungwoochoi/data/axis_rag/data/ms_marco_embedding.npy", passage_embeddings['dense_vecs'])
